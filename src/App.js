09fea399ffc88d6e28a5ad3ba8f6bbf5f69350e5
Santi-Uriguen <sanituriguen@hotmail.com>
2021-05-04 00:55:07 -0300

    obile styles

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Header from "./components/header/Header";
 import Main from "./components/main/Main";
 import Footer from "./components/footer/Footer"
 import "./styles/App.css";
+import "./styles/MobileApp.css";
 
 function App() {
   return (
